55cb27a4b5d7e22fd71ae441fa212952fa45f2a2
toru173 <toru173+github@gmail.com>
2024-11-02 22:23:18 +1000

    Fixing package errors

--- adjust_line_endings/__init__.py
+++ adjust_line_endings/__init__.py
@@ -28,6 +28,6 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from polyglot import main
+from .adjust_line_endings import main
 
 __all__ = ['main']
--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='adjust_line_endings.py',
-    version='0.5',
-    author='toru173',
-    description='A Python module for working with polyglot files',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/toru173/polyglot',
-    packages=find_packages(),
-    python_requires='>=3.7',
+    name = 'adjust_line_endings',
+    version = '0.5',
+    author = 'toru173',
+    description = 'A Python module for working with polyglot files',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/toru173/polyglot',
+    packages = ['adjust_line_endings'],
+    entry_points = {
+        'console_scripts': [
+            'adjust_line_endings = adjust_line_endings.adjust_line_endings.py:main',
+        ],
+    }
 )
