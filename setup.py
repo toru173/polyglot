9b8b3aba8bc18a89077da4940c0759c166f51fc1
toru173 <toru173+github@gmail.com>
2024-11-02 16:46:06 +1000

    Correct path to executable

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='adjust_line_endings.py',
-    version='0.3',
+    version='0.4',
     author='toru173',
     description='A Python module for working with polyglot files',
     long_description=open('README.md').read(),
