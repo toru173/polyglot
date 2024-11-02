5972e9420d97c5d6194072ff87abda377f34c003
toru173 <toru173+github@gmail.com>
2024-11-02 16:24:39 +1000

    Create Package Package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adjust_line_endings.py',
+    version='0.1',
+    author='toru173',
+    description='A Python module for working with polyglot files',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/toru173/polyglot',
+    packages=find_packages(),
+    python_requires='>=3.7',
+)
